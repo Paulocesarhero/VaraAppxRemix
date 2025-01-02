391e6262b446e78f00951dbe7a5faacffbd96f74
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2025-01-02 11:30:33 -0600

    poc of drizzle orm

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,66 @@
-import { Redirect } from "expo-router";
-import React from "react";
+import { useLiveQuery, drizzle } from "drizzle-orm/expo-sqlite";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import { ScrollView, Text, View, Button, Alert } from "react-native";
+import { AvisoValues } from "varaapplib/components/AvisoForm/types";
 
-import useSettingStore from "../hooks/globalState/useSettingStore";
-import LoginPage from "./screens/Login/LoginPage";
+import { db } from "../database/connection/sqliteConnection";
+import migrations from "../database/migrations/drizzle/migrations";
+import { addAviso } from "../database/repsitory/avisoRepo";
+import { avisos } from "../database/schemas/avisoSchema";
 
-export default function App() {
-  const { isLoggedIn } = useSettingStore();
+const App = () => {
+  const avisosToAgregate: AvisoValues = {
+    Nombre: "Paulito",
+    Telefono: "2282522839",
+    FacilAcceso: false,
+    Acantilado: false,
+    Sustrato: 0,
+    LugarDondeSeVio: 0,
+    FechaDeAvistamiento: "",
+    Observaciones: "",
+    CondicionDeAnimal: 0,
+    CantidadDeAnimales: "",
+    InformacionDeLocalizacion: "",
+    Latitud: "12",
+    Longitud: "12",
+    Fotografia: null,
+  };
 
-  return isLoggedIn ? (
-    <Redirect href="Home/Recommendations/Recommendations" />
-  ) : (
-    <LoginPage />
+  const addAvisoHandler = async () => {
+    try {
+      await addAviso(avisosToAgregate, "nombreAvisoPlaceholder");
+      Alert.alert("Éxito", "El aviso se ha agregado correctamente.");
+    } catch (error) {
+      // @ts-ignore
+      Alert.alert("Error", "No se pudo agregar el aviso: " + error.message);
+    }
+  };
+
+  // Re-renders automatically when data changes
+  const { data } = useLiveQuery(db.select().from(avisos));
+  const { success, error } = useMigrations(db, migrations);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView>
+      <Button title="Agregar Aviso" onPress={addAvisoHandler} />
+      <Text>{JSON.stringify(data, null, 2)}</Text>
+    </ScrollView>
   );
-}
+};
+
+export default App;
