7c3fe7b53d02942db97b054d37695291b774363e
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2025-01-03 00:12:58 -0600

    fix sql connection imports

--- src/database/connection/sqliteConnection.ts
+++ src/database/connection/sqliteConnection.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/expo-sqlite/index";
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import { openDatabaseSync } from "expo-sqlite";
 
 const expo = openDatabaseSync("db.db", { enableChangeListener: true }); // <-- enable change listeners
