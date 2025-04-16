2e039933837f72a1bfeeff97a3ac1d0055c0448d
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2025-04-15 21:06:17 -0600

    Fix update border radius style in LabelAndImage component

--- src/components/LabelAndImage/LabelAndImage.tsx
+++ src/components/LabelAndImage/LabelAndImage.tsx
@@ -26,7 +26,7 @@ const LabelAndImage: React.FC<InputAndImageProps> = ({ image, label }) => {
             color="black"
             style={{
               boxShadow: "0px 2px 7px rgba(0, 0, 0, 0.9)",
-              borderRadius: "100%",
+              borderRadius: 15,
             }}
           />
         </TouchableOpacity>
